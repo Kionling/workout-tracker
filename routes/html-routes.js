3a845021a1a1bb135a7b42802100c724224bcee2
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-07 09:08:06 -0700

    Forms are functional abut are not saving information from forms. Mongo DB has not been created from this point

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const Transaction = require("../models/workout.js");
+const path = require("path");
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(__dirname, "../public/exercise.html")
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+})
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
 
